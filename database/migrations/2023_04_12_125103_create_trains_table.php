5f3a752ce35688e5a05f700ce4f89ae5c1fb6f86
Ant0C <cafeo97@gmail.com>
2023-04-12 15:46:58 +0200

    Insert table with column

---
+++ database/migrations/2023_04_12_125103_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company',20);
+            $table->string('departure_station',50);
+            $table->string('arrival_station',50);
+            $table->dateTime('arrival_time',$precision = 0);
+            $table->dateTime('departure_time',$precision = 0);
+            $table->unsignedMediumInteger('train_code');
+            $table->unsignedTinyInteger('number_of_carriages');
+            $table->unsignedTinyInteger('delay_time');
+            $table->string('status',25);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};

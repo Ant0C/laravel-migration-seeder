9f803389c7be506a0f3f87beb059aaddfef4c319
Ant0C <cafeo97@gmail.com>
2023-04-12 17:59:52 +0200

    stamp on screen only train of today

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -5,16 +5,24 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use App\Models\Train;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class PageController extends Controller
 {
     public function index() 
     {
-        $trains = Train::all();
+        $date = Carbon::today()->toDateTimeString();
+        //dd($date);
+
+        $cur_date = Carbon::createFromFormat('Y-m-d H:i:s', $date)->format('Y-m-d');
+        //dd($cur_date);
+
+        $trains = Train::whereDate('departure_time','=', $cur_date)->get();
         //dd($trains);
         $data = [
             'trains'=> $trains
         ];
         return view ('welcome',$data);
     }
+
 }

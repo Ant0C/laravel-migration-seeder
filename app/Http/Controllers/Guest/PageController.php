6b56af21c1cb4ec0abc53f65c0afa83af73a1bdb
Ant0C <cafeo97@gmail.com>
2023-04-12 16:51:18 +0200

    Stamp all trains on screen

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use App\Models\Train;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index() 
+    {
+        $trains = Train::all();
+        //dd($trains);
+        $data = [
+            'trains'=> $trains
+        ];
+        return view ('welcome',$data);
+    }
+}
